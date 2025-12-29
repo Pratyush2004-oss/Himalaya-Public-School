2a8433c78039c926c2f1a32351a05e1b057bb980
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-12-29 22:18:12 +0530

    Adding newSection

---
+++ mobile/app/editProfile/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EditProfilePage = () => {
+  return (
+    <View>
+      <Text>EditProfilePage</Text>
+    </View>
+  )
+}
+
+export default EditProfilePage;
