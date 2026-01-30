099c170db3eea867954a522d7ca95926d1382c16
Pratyush2004-oss <pratyush@sitp.ac.in>
2026-01-30 23:30:58 +0530

    Adding changes

--- mobile/components/editProfile/EditProfileSection.tsx
+++ mobile/components/editProfile/EditProfileSection.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import ChangePasswordDialog from './ChangePasswordDialog';
 
 const EditProfileSection = () => {
   return (
     <View>
       <Text>EditProfileSection</Text>
+      <ChangePasswordDialog/>
     </View>
   )
 }
